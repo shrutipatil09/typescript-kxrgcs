1b6471b1c67f320fbae1b5e370b626f1b81b8271
shrutipatil09 <shruti.sanjay.patil@accenture.com>
2020-01-23 11:54:33 +0530

    1 file modified, 1 added

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 // Import stylesheets
 import './style.css';
-var Excel=require("exceljs");
-var wb=new Excel.Workbook();
+var Excel = require("exceljs");
+var wb = new Excel.Workbook();
 wb.xlsx.readFile("./sample.xlsx").then(function () {
-var sh=wb.getWorksheet("Sheet1");
+var sh = wb.getWorksheet("Sheet1");
 console.log(sh.rowCount);
 for (var i=1; i<=sh.rowCount; i++) {
 console.log(sh.getRow(i).getCell(1).value);
