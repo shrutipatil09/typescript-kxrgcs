2ef370fdd93802b570457a9c014da97e5984a787
shrutipatil09 <shruti.sanjay.patil@accenture.com>
2020-01-31 18:30:30 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,13 +1,40 @@
-// Import stylesheets
-import './style.css';
-var Excel = require("exceljs");
+"use strict";
+exports.__esModule = true;
+var Excel = require('exceljs');
+var XLSX = require("xlsx");
 var wb = new Excel.Workbook();
-wb.xlsx.readFile("./sample.xlsx").then(function () {
-var sh = wb.getWorksheet("Sheet1");
-console.log(sh.rowCount);
-for (var i=1; i<=sh.rowCount; i++) {
-console.log(sh.getRow(i).getCell(1).value);
-console.log(sh.getRow(i).getCell(2).value);
+var wb1 = XLSX.readFile("./DummyData.xlsx");
+var sheetNames1 = wb1.SheetNames;
+var W1WorkSheets = [];
+wb.xlsx.readFile("./DummyData.xlsx").then(function () {
+    var sheetName1 = sheetNames1[0];
+    W1WorkSheets.push(wb.getWorksheet(sheetName1));
+    for (var i = 1; i < sheetNames1.length; ++i) {
+        var sheetName = sheetNames1[i];
+        W1WorkSheets.push(wb.getWorksheet(sheetName));
     }
+    fi();
 });
-
+function fi() {
+    var flag = 0;
+    var dub = 0;
+    var arrayOfEror = [];
+    for (var i = 1; i < sheetNames1.length; ++i) {
+        for (var a = 2; a <= W1WorkSheets[i].rowCount; a++) {
+            if (W1WorkSheets[0].getRow(a).getCell(1).value != W1WorkSheets[i].getRow(a).getCell(1).value) {
+                flag = 1;
+                var error = sheetNames1[0] + ' and ' + sheetNames1[i] + ' are diffrent at row ' + (a) + ', column ' + (1);
+                arrayOfEror.push(error);
+            }
+        }
+    }
+    if (flag == 1) {
+        for (var i= 0; i < arrayOfEror.length; i++) {
+            console.log(arrayOfEror[i]);
+        }
+    }
+    if (flag == 0) {
+        console.log("All TC's are same in Business Flow and its keyword's sheets");
+    }
+}
+;
